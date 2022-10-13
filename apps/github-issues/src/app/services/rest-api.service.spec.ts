fab7fa7871edc4ce58bf5417a72726573f86a5ad
ArianeGutierrez <arianeguti10@gmail.com>
2022-10-13 11:21:17 +0200

    Test RestApiService

--- apps/github-issues/src/app/services/rest-api.service.spec.ts
+++ apps/github-issues/src/app/services/rest-api.service.spec.ts
@@ -1,16 +1,27 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { RestApiService } from './rest-api.service';
 
 describe('RestApiService', () => {
   let service: RestApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RestApiService]
+    });
     service = TestBed.inject(RestApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('should be return empty array', () => {
+    const url = "https://github.com/user/repository"    // repositorio no existente
+    service.getRepositoryIssues(url, 5, 1).subscribe(res => {
+      expect(res).toEqual([])
+    })
   });
 });
